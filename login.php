e4799e5c91d3dc13f92710eba4655f69f8e0d49c
zainpelope <zain.flutter@gmail.com>
2024-11-24 22:30:41 +0700

    update form login

--- login.php
+++ login.php
@@ -3,138 +3,123 @@ session_start();
 include 'dbconnect.php';
 $alert = '';
 
-if(isset($_SESSION['role'])){
+if (isset($_SESSION['role'])) {
 	$role = $_SESSION['role'];
 
-	if($role=='Admin'){
+	if ($role == 'Admin') {
 		header("location:admin/");
 	} else {
 		header("location:user/");
 	}
-	
 }
 
+if (isset($_POST['btn-login'])) {
+	$email = mysqli_real_escape_string($conn, $_POST['email']);
+	$password = mysqli_real_escape_string($conn, $_POST['password']);
+	$cariadmin = mysqli_query($conn, "SELECT * FROM admin WHERE adminemail='$email' AND adminpassword='$password';");
+	$cariuser = mysqli_query($conn, "SELECT * FROM user WHERE useremail='$email' AND userpassword='$password';");
 
-if(isset($_POST['btn-login']))
-{
- $email = mysqli_real_escape_string($conn,$_POST['email']);
- $password = mysqli_real_escape_string($conn,$_POST['password']);
+	$cekadmin = mysqli_num_rows($cariadmin);
+	$cekuser = mysqli_num_rows($cariuser);
 
- // menyeleksi data user dengan username dan password yang sesuai
-$cariadmin = mysqli_query($conn,"select * from admin where adminemail='$email' and adminpassword='$password';");
-$cariuser = mysqli_query($conn,"select * from user where useremail='$email' and userpassword='$password';");
-
-// menghitung jumlah data yang ditemukan
-$cekadmin = mysqli_num_rows($cariadmin);
-$cekuser = mysqli_num_rows($cariuser);
- 
-// cek apakah username dan password di temukan pada database
-	if($cekadmin > 0){
-	
-	//jika admin
-	$data = mysqli_fetch_assoc($cariadmin);
- 
-		// buat session login dan username
+	if ($cekadmin > 0) {
+		$data = mysqli_fetch_assoc($cariadmin);
 		$_SESSION['email'] = $data['adminemail'];
 		$_SESSION['role'] = "Admin";
 		header("location:admin");
- 	} else if($cekuser > 0){
-	//jika user biasa
-	$data = mysqli_fetch_assoc($cariuser);
- 
-		// buat session login dan username
+	} elseif ($cekuser > 0) {
+		$data = mysqli_fetch_assoc($cariuser);
 		$_SESSION['email'] = $data['useremail'];
 		$_SESSION['userid'] = $data['userid'];
 		$_SESSION['role'] = "User";
 		header("location:user");
 	} else {
-	//jika user tidak ditemukan
-	echo "<div class='alert alert-warning'>Username atau Password salah</div>
-    <meta http-equiv='refresh' content='2'>";
+		echo "<div class='alert alert-warning'>Username atau Password salah</div>
+        <meta http-equiv='refresh' content='2'>";
 	}
- 
 }
-
-
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Masuk</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-144808195-1"></script>
-	<script>
-	  window.dataLayer = window.dataLayer || [];
-	  function gtag(){dataLayer.push(arguments);}
-	  gtag('js', new Date());
 
-	  gtag('config', 'UA-144808195-1');
-	</script>
-    <script src="jquery.min.js"></script>
-	<style>body{background-color:#17a2b8;}
-	@media screen and (max-width: 600px) {
-h4{font-size:85%;}
-}
-.container{
-	background-color:#2c3e50;
-	width:70%;
-	border: 3px white;
-	border-style:solid;
-	border-radius:30px;
-	padding-left:10%;
-	padding-right:10%;
-	padding-top:3%;
-	padding-bottom:2%;
-}
-.btn{
-	width:40%;
-}
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Login - MTS Nurul Karomah</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	<style>
+		body {
+			background-color: #f7f7f7;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			min-height: 100vh;
+			margin: 0;
+		}
+
+		.login-container {
+			background-color: #ffffff;
+			border-radius: 15px;
+			box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.1);
+			padding: 30px;
+			text-align: center;
+			width: 100%;
+			max-width: 400px;
+		}
+
+		.login-container img {
+			max-width: 100px;
+			margin-bottom: 15px;
+		}
+
+		.show-password {
+			position: absolute;
+			right: 10px;
+			top: 50%;
+			transform: translateY(-50%);
+			cursor: pointer;
+		}
 	</style>
-	<link rel="icon" 
-      type="image/png" 
-      href="favicon.png">
-  </head>
-  <body>
+</head>
+
+<body>
+	<div class="login-container">
+		<img src="nurul.png" alt="School Logo" style="width: 150px; height: auto;">
 
 
-  <div align="center">
-  
-  <div style = "color:white">
-	<h1>Selamat Datang</1>
-</div>
+		<h3>Selamat Datang di MTS Nurul Karomah</h3>
+		<p class="text-muted">Silakan login untuk mengakses sistem</p>
+		<form method="post" style="position: relative;">
+			<div class="form-group mb-3 position-relative">
+				<input type="email" class="form-control" placeholder="Email" name="email" required autofocus>
+			</div>
+			<div class="form-group mb-3 position-relative">
+				<input type="password" class="form-control" placeholder="Password" name="password" id="password" required>
+				<span class="show-password" onclick="togglePassword()">
+					<i class="bi bi-eye" id="toggle-icon"></i>
+				</span>
+			</div>
+			<button type="submit" class="btn btn-primary btn-block w-100 mb-2" name="btn-login">Masuk</button>
+			<a class="btn btn-info w-100" href="register.php">Daftar</a>
+		</form>
+	</div>
+	<script>
+		function togglePassword() {
+			const passwordField = document.getElementById('password');
+			const toggleIcon = document.getElementById('toggle-icon');
+			if (passwordField.type === 'password') {
+				passwordField.type = 'text';
+				toggleIcon.classList.remove('bi-eye');
+				toggleIcon.classList.add('bi-eye-slash');
+			} else {
+				passwordField.type = 'password';
+				toggleIcon.classList.remove('bi-eye-slash');
+				toggleIcon.classList.add('bi-eye');
+			}
+		}
+	</script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.js"></script>
+</body>
 
-
-	<br \><br \>
-			<div class="container">
-					<div style="color:white">
-					<label>Login</label><br \>
-					
-					</div>
-                <form method="post">
-                    <div class="form-group">
-                        <input type="email" class="form-control" placeholder="Email" name="email" autofocus required>
-                    </div>
-                    <div class="form-group">
-                        <input type="password" class="form-control" placeholder="Password" name="password" required>
-                    </div>
-                    <button type="submit" class="btn btn-primary" name="btn-login">Masuk</button>
-					<a class="btn btn-info text-light" href="register.php">Daftar</a>
-                </form>
-			
-			<br \>
-        </div></div>
-       
-     
-	
-	
-  </body>
 </html>
